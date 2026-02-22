3a9ec5f247b9ba5d0c28e0f20069715fa0f6ee4c
enuila1325 <enuila634@gmail.com>
2026-02-22 11:22:48 -0600

    navbar color and UX fixed according to client demmand

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -11,29 +11,60 @@ import { useState } from "react";
 import CartDrawer from "./CartDrawer";
 import { useCart } from "../../context/CartContext";
 
-
 export default function Navbar({ darkMode, setDarkMode }) {
     const [open, setOpen] = useState(false);
     const { cart } = useCart();
 
-
     return (
         <>
-            <AppBar position="sticky">
+            <AppBar
+                position="sticky"
+                elevation={0}
+                sx={{
+                    backgroundColor: "#1f1f1f",   // gris negro fijo
+                    borderBottom: "1px solid #2a2a2a",
+                }}
+            >
                 <Toolbar>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            flexGrow: 1,
+                            fontWeight: 600,
+                            letterSpacing: 1,
+                        }}
+                    >
                         SkinLove
                     </Typography>
-                    <IconButton color="inherit" onClick={() => setDarkMode(!darkMode)}>
+
+                    {/* Dark Mode Toggle */}
+                    <IconButton
+                        sx={{ color: "#fff" }}
+                        onClick={() => setDarkMode(!darkMode)}
+                    >
                         <DarkModeIcon />
                     </IconButton>
-                    <IconButton color="inherit" onClick={() => setOpen(true)}>
-                        <Badge badgeContent={cart.length} color="secondary">
+
+                    {/* Cart */}
+                    <IconButton
+                        sx={{ color: "#fff" }}
+                        onClick={() => setOpen(true)}
+                    >
+                        <Badge
+                            badgeContent={cart.length}
+                            sx={{
+                                "& .MuiBadge-badge": {
+                                    backgroundColor: "#c48b9f", // rosado marca
+                                    color: "#fff",
+                                },
+                            }}
+                        >
                             <ShoppingCartIcon />
                         </Badge>
                     </IconButton>
                 </Toolbar>
             </AppBar>
+
             <CartDrawer open={open} onClose={() => setOpen(false)} />
         </>
     );
